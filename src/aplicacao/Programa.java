898d70dd1bd62b5ebdc2a2319a7f5245bf3417a7
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-09-14 13:23:49 -0300

    classe Posicao

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,10 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+  public static void main(String[] args) {
+    Posicao posicao = new Posicao(1, 1);
+    System.out.println(posicao);
+  }
+}
